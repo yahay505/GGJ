606888916c0463f042ccb28676258d4730dec055
yavuz <yavuzoyun505@gmail.com>
2022-01-22 00:33:20 +0300

    day1

---
+++ Assets/Script/Card.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace Script
+{
+    [CreateAssetMenu(fileName = "New Card", menuName = "Create Card", order = 0)]
+    public class Card : ScriptableObject
+    {
+        public Sprite Image;
+        public string Text;
+        public bool IsAction;
+        public int MoneyEffect, PopularityEffect;
+        #region effects
+
+        #region money
+
+        public bool x0;
+        public int MoneyInstant;
+        
+        public bool x1;
+        public int MoneyPerTurn,MoneyTurnCount;
+        
+        public bool x2;
+        public float MoneyMultiplierForDay=1;
+        
+        public bool x3;
+        public float MoneyMultiplierForXTurns;
+        public int MoneyMultiplierTurnCount;
+
+        public bool x4;
+        public int MoneyReturnAfterTurn, MoneyReturnAfterTurnCount;
+
+        public bool x5;
+        public float MoneyNextCardGainMultiplier;
+        
+        public bool x6;
+        public int MoneyNextCardGain;
+
+        #endregion
+
+
+        #region Popularity
+        
+        public int PopularityInstant;
+        public int PopularityPerTurn,PopularityTurnCount;
+        public float PopularityMultiplierForDay;
+        
+        public float PopularityMultiplierForXTurns;
+        public int PopularityMultiplierTurnCount;
+
+        public int PopularityReturnAfterTurn, PopularityReturnAfterTurnCount;
+
+        public float PopularityNextCardGainMultiplier;
+        public int PopularityNextCardGain;
+
+
+        #endregion
+        #endregion
+    }
+}
---
+++ Assets/Script/DragnDrop.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+[RequireComponent(typeof(Collider2D))]
+public class DragnDrop : MonoBehaviour
+{
+    private Camera mainCam;
+    void Start()
+    {
+        mainCam=Camera.main;
+    }
+
+
+    
+    private void OnMouseDown()
+    {
+        StopAllCoroutines();
+
+        StartCoroutine(twine(1.1f,1.3f));
+
+        
+    }
+    private void OnMouseUp()
+    {
+        StopAllCoroutines();
+        StartCoroutine(twine(1.3f,1.1f));
+
+    }
+
+
+
+    private void OnMouseDrag()
+    {
+        var x=mainCam.ScreenToWorldPoint(Input.mousePosition);
+        x.z = transform.position.z;
+        transform.position =x ;
+    }
+
+    private void OnMouseEnter()
+    {
+        StopAllCoroutines();
+        StartCoroutine(twine(1.0f, 1.1f));
+
+
+    }
+    //
+    private void OnMouseExit()
+    {
+        StopAllCoroutines();
+        StartCoroutine(twine(1.1f, 1.0f));
+    }
+    
+        IEnumerator twine(float a,float b)
+        {
+           
+            for (int i = 0; i < 30; i++)
+            {
+                transform.localScale = Vector3.one* math.remap(0f, 29f, a, b, i);
+                yield return null;
+            }
+        }
+    
+}
