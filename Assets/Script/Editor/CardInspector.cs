aec5920eddc57cfc335f32b2b0bc9991ed6cb04e
SaranBaran <69807636+SaranBaran@users.noreply.github.com>
2022-01-22 12:39:30 +0300

    Revert "Merge branch 'master' of https://github.com/yahay505/GGJ"
    This reverts commit 7da424798c0f5464fb766e7e05a25149b36f9268, reversing
    changes made to c15b5010273c82e90dfd5f2072151a843c97b4b6.

--- Assets/Script/Editor/CardInspector.cs
+++ Assets/Script/Editor/CardInspector.cs
@@ -75,48 +75,48 @@ namespace Script.Editor
             if (card.usePop)
             {
                 
-                card.y0 = EditorGUILayout.Toggle("Has Instant Popularity", card.y0);
-                if  (card.y0)
+                card.x0 = EditorGUILayout.Toggle("Has Instant Popularity", card.x0);
+                if  (card.x0)
                 {
                     card.PopularityInstant = EditorGUILayout.IntField("amount", card.PopularityInstant);
                 }
 
-                card.y1 = EditorGUILayout.Toggle("Has Popularity Per Turn", card.y1);
-                if  (card.y1)
+                card.x1 = EditorGUILayout.Toggle("Has Popularity Per Turn", card.x1);
+                if  (card.x1)
                 {
                     card.PopularityPerTurn = EditorGUILayout.IntField("amount", card.PopularityPerTurn);
                     card.PopularityTurnCount = EditorGUILayout.IntField("turns", card.PopularityTurnCount);
                 }
                 
-                card.y2 = EditorGUILayout.Toggle("Has Daily Popularity Multiplier", card.y2);
-                if  (card.y2) 
+                card.x2 = EditorGUILayout.Toggle("Has Daily Popularity Multiplier", card.x2);
+                if  (card.x2) 
                 {
                     card.PopularityMultiplierForDay = EditorGUILayout.FloatField("amount", card.PopularityMultiplierForDay);
                 }
                 
-                card.y3 = EditorGUILayout.Toggle("Has Popularity Multiplier for turn", card.y3);
-                if  (card.y3) 
+                card.x3 = EditorGUILayout.Toggle("Has Popularity Multiplier for turn", card.x3);
+                if  (card.x3) 
                 {
                     card.PopularityMultiplierForXTurns = EditorGUILayout.FloatField("amount", card.PopularityMultiplierForXTurns);
                     card.PopularityMultiplierTurnCount = EditorGUILayout.IntField("turns", card.PopularityMultiplierTurnCount);
 
                 }
                 
-                card.y4 = EditorGUILayout.Toggle("Has Popularity Return After Turn", card.y4);
-                if  (card.y4) 
+                card.x4 = EditorGUILayout.Toggle("Has Popularity Return After Turn", card.x4);
+                if  (card.x4) 
                 {
                     card.PopularityReturnAfterTurn = EditorGUILayout.IntField("amount", card.PopularityReturnAfterTurn);
                     card.PopularityReturnAfterTurnCount = EditorGUILayout.IntField("turn", card.PopularityReturnAfterTurnCount);
                 }            
                 
-                card.y5 = EditorGUILayout.Toggle("Has Popularity multiplier for next action", card.y5);
-                if  (card.y5) 
+                card.x5 = EditorGUILayout.Toggle("Has Popularity multiplier for next action", card.x5);
+                if  (card.x5) 
                 {
                     card.PopularityNextCardGainMultiplier = EditorGUILayout.FloatField("amount", card.PopularityNextCardGainMultiplier);
                 }
 
-                card.y6 = EditorGUILayout.Toggle("Has Popularity Gain on next action", card.y6);
-                if  (card.y6) 
+                card.x6 = EditorGUILayout.Toggle("Has Popularity Gain on next action", card.x6);
+                if  (card.x6) 
                 {
                     card.PopularityNextCardGain = EditorGUILayout.IntField("amount", card.PopularityNextCardGain);
                 }
